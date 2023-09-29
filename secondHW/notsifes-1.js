33b871b7e3239508aa5be1d7ebab9afe9b9b98e3
sifes <lonfark@gmail.com>
2023-09-29 22:36:01 +0300

    first and last tasks

--- secondHW/notsifes-1.js
+++ secondHW/notsifes-1.js
@@ -1,5 +1,18 @@
-function name(params) {
+function council(arr, d) {
+    let sum = 0;
+    let tempSum = 0;
+    let space = Math.floor(arr.length / d);;
 
+    for (let i = 0; i < space; i++) {
+        tempSum = 0;
+        for (let j = i; j < arr.length; j += space) {
+            tempSum += arr[j];
+        }
+        if (tempSum > sum) {
+            sum = tempSum;
+        }
+    }
+    return sum;
 }
 
-console.log(name());
+console.log(council([1, 5, 6, 3, 4, 2], 3)); //6
--- secondHW/notsifes-6.js
+++ secondHW/notsifes-6.js
@@ -1,5 +1,31 @@
-function name(params) {
+function landPerimeter(arr) {
+    let res = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length; j++) {
+            if (arr[i][j] === 'X') {
+                res += 4;
+            }
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[0].length - 1; j++) {
+            if (arr[i][j] === 'X' && arr[i][j + 1] === 'X') {
+                res -= 2;
+            }
+            if (i + 1 < arr.length && arr[i][j] === 'X' && arr[i + 1][j] === 'X') {
+                res -= 2;
+            }
+        }
+    }
+
+    return res;
 }
 
-console.log(name());
+console.log(landPerimeter(
+    ['XOOO',
+        'XOXO',
+        'XOXO',
+        'OOXX',
+        'OOOO'])); // 18
